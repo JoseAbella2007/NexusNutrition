fbc1ea4341294782b76d836a695d18d35ae85b96
MarianoDeFilippo <marianodf62@gmail.com>
2026-09-10 17:39:57 -0300

    Se agrego carpeta del diseno y lo conecto a la pagina de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { RUTAS } from "./routes/rutas";
+import DisenoSitio from "./components/common/DisenoSitio";
 import Inicio from "./components/pages/Inicio";
 import Login from "./components/pages/Login";
 import Registro from "./components/pages/Registro";
@@ -8,7 +9,10 @@ import Error404 from "./components/pages/Error404";
 export default function App() {
   return (
     <Routes>
-      <Route path={RUTAS.INICIO} element={<Inicio />} />
+      <Route element={<DisenoSitio />}>
+        <Route path={RUTAS.INICIO} element={<Inicio />} />
+      </Route>
+
       <Route path={RUTAS.INICIAR_SESION} element={<Login />} />
       <Route path={RUTAS.REGISTRO} element={<Registro />} />
       <Route path={RUTAS.NO_ENCONTRADA} element={<Error404 />} />
