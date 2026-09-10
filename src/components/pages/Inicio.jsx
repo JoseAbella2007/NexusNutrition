eeb886f5571c796f7378d213815b6a2f679ced71
MarianoDeFilippo <marianodf62@gmail.com>
2026-09-10 20:11:41 -0300

    Se agrega la seccion de filosofia en pagina inicio

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -3,6 +3,7 @@ import InsigniasConfianza from "./secciones/InsigniasConfianza";
 import Categorias from "./secciones/Categorias";
 import Beneficios from "./secciones/Beneficios";
 import Proceso from "./secciones/Proceso";
+import Filosofia from "./secciones/Filosofia";
 import DivisorSeccion from "../common/DivisorSeccion";
 
 export default function Inicio() {
@@ -14,6 +15,7 @@ export default function Inicio() {
       <DivisorSeccion frase="CIENCIA · DISCIPLINA · EVOLUCIÓN" />
       <Beneficios />
       <Proceso />
+      <Filosofia />
     </main>
   );
 }
