ab18d3f43d09625f6c4357f2cfba235b8626404a
MarianoDeFilippo <marianodf62@gmail.com>
2026-09-10 18:57:53 -0300

    elimino page 404 y vinculacion en app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import DisenoSitio from "./components/common/DisenoSitio";
 import Inicio from "./components/pages/Inicio";
 import Login from "./components/pages/Login";
 import Registro from "./components/pages/Registro";
-import Error404 from "./components/pages/Error404";
 
 export default function App() {
   return (
@@ -15,8 +14,6 @@ export default function App() {
 
       <Route path={RUTAS.INICIAR_SESION} element={<Login />} />
       <Route path={RUTAS.REGISTRO} element={<Registro />} />
-      <Route path={RUTAS.NO_ENCONTRADA} element={<Error404 />} />
-      <Route path="*" element={<Error404 />} />
     </Routes>
   );
 }
--- src/components/pages/Error404.jsx
+++
@@ -1,3 +0,0 @@
-export default function Error404() {
-  return <h1>Error404</h1>;
-}
