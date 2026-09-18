5c33814faa740e12b161fcc696cb00993ae28ac0
JoséAbella <piratatab2015@gmail.com>
2026-09-17 23:23:46 -0300

    Agrega una correccion en el diseño del SideCarrito

--- src/components/common/SideCarrito.jsx
+++ src/components/common/SideCarrito.jsx
@@ -33,23 +33,24 @@ return (
         />
     )}
     <aside
-        className={`
-        fixed top-0 right-0 h-full w-100
-        bg-[#080808]
-        border-l border-[#39ff14]/30
-        z-201
-        transform transition-transform duration-300 ease-in-out
-        ${abierto ? "translate-x-0" : "translate-x-full"}
-        `}
+    className={`
+    fixed top-0 right-0 h-full w-100
+    bg-[#080808]
+    border-l-4 border-[#7b2cff]/30
+    shadow-[-12px_0_40px_rgba(168,85,247,0.35)]
+    z-201
+    transform transition-all duration-300 ease-in-out
+    ${abierto ? "translate-x-0" : "translate-x-full"}
+`}
     >
-        <div className="flex items-center justify-center p-5 bg-[#0d0d0d] border-b border-[#39ff14]/20">
+        <div className="flex items-center justify-center p-5 bg-[#0d0d0d] border-b-3 border-[#7b2cff]/30">
         <div>
             <h2 className="text-xl font-bold text-white">
             Carrito de Compras
             </h2>
-            <p className="text-sm text-(purple-2) ">  
-            {cantidadProductos}{" "}
-            {cantidadProductos === 1 ? "producto" : "productos"}
+            <p className="flex items-center justify-center text-sm text-[#a855f7]! gap-2">  
+            <span>{cantidadProductos}</span>
+            <span>{cantidadProductos === 1 ? "Producto" : "Productos"}</span>
             </p>
         </div>
         <button
@@ -59,7 +60,7 @@ return (
             <i className="fas fa-times"></i>
         </button>
         </div>
-        <div className="p-5 overflow-y-auto h-[calc(100%-180px)]">
+        <div className="p-6 overflow-y-auto h-[calc(100%-180px)]">
         {carrito.length === 0 ? (
             <p className="text-gray-400 text-center">
             Tu carrito está vacío
@@ -69,12 +70,12 @@ return (
             {carrito.map((producto) => (
                 <div
             key={producto.id}
-            className="flex gap-3 p-3 rounded-lg bg-[#0d0d0d]"
+            className="flex gap-4 p-4 rounded-lg bg-[#0d0d0d]"
                 >
     <img
     src={producto.imagen}
     alt={producto.nombre}
-    className="w-20 h-20 object-cover rounded-md"
+    className="w-20 h-20 m-2 object-cover rounded-md border-red-500"
     />
 
     <div className="flex-1 min-w-0">
@@ -114,12 +115,12 @@ return (
         </div>
         )}
         </div>
-        <div className="absolute bottom-0 left-0 w-full p-5 bg-[#0d0d0d] border-t border-[#39ff14]/20">
+        <div className="absolute bottom-0 left-0 w-full p-5 bg-[#0d0d0d] border-t-3 border-[#7b2cff]/30">
         <div className="flex justify-between items-center mb-4">
         <span className="text-gray-400">
         Total
         </span>
-        <span className="text-xl font-bold text-[#39ff14]">
+        <span className="text-xl font-bold text-[#8a8a8a]">
         ${total.toLocaleString("es-AR")}
         </span>
         </div>
@@ -127,7 +128,6 @@ return (
             className="
             w-full
             py-3
-            rounded-lg
             bg-[#39ff14]
             text-[#050505]
             font-bold
