02a988ca28c7cc6528ef23ffb078f8ad3aaccc71
mdpilarmolina <mdpilarmolina@gmail.com>
2026-09-18 02:47:46 -0300

    Agrega archivos QuickViewProducto como modal a ver detalle de producto. Elimina archivos y Detalledeproducto. Falta darle mas diseño.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Login from "./components/pages/Login";
 import Registro from "./components/pages/Registro";
 import Administrador from "./components/pages/Administrador";
 import CategoriaProductos from "./pages/productos/CategoriaProductos";
-import DetalleDeProducto from "./pages/productos/DetalleDeProducto";
 import AdminInicio from "./components/pages/admin/AdminInicio";
 import AdminUsuarios from "./components/pages/admin/AdminUsuarios";
 import AdminProductos from "./pages/productos/AdminProductos";
@@ -22,7 +21,6 @@ export default function App() {
           path="/categoria/:nombreCategoria"
           element={<CategoriaProductos />}
         />
-        <Route path="/producto/:id" element={<DetalleDeProducto />} />
 
         <Route element={<RutaAdministrador />}>
           <Route path={RUTAS.ADMIN} element={<Administrador />}>
